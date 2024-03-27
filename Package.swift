3a3609a492c76c30d3fc46ab42af09c5fada7ca7
yusuf.ozgul <yusuf.ozgul@trendyol.com>
2024-03-27 09:11:24 +0300

    Update minimum iOS Version

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "MockingStar",
     platforms: [
-        .iOS(.v15),
+        .iOS(.v13),
         .macOS(.v10_15)
     ],
     products: [
